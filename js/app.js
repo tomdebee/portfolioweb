dfda01400a77183438446bcdd9acf3e195b5126c
Tom van Bavel <tvbavel@sogyo.nl>
2017-11-22 16:22:37 +0100

    updated with separate controller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 // https://thinkster.io/tutorials/mean-stack/jumping-in-with-angular
+
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+    '$scope',
+    function($scope){
+        $scope.test = 'Hello world!';
+}]);
